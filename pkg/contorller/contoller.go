4c7e419bf515d5e1440a50e9ef58d4749eed0d71
OyutianO <510693455@qq.com>
2021-07-01 21:23:08 +0800

    some name optimizations

--- pkg/contorller/contoller.go
+++ pkg/contorller/contoller.go
@@ -55,17 +55,17 @@ func NewPyTorchExtensionController(queueInformer queueInformers.QueueUnitInforme
 	}
 }
 
-func (t *PyTorchExtensionController) Run(threadiness int, stopCh <-chan struct{}) error {
+func (pc *PyTorchExtensionController) Run(threadiness int, stopCh <-chan struct{}) error {
 	defer runtime.HandleCrash()
-	defer t.workqueue.ShutDown()
+	defer pc.workqueue.ShutDown()
 
 	klog.Info("Start PyTorchExtensionController Run function")
-	if !cache.WaitForCacheSync(stopCh, t.queueInformer.Informer().HasSynced) {
+	if !cache.WaitForCacheSync(stopCh, pc.queueInformer.Informer().HasSynced) {
 		return fmt.Errorf("failed to wait for caches to sync")
 	}
 
 	for i := 0; i < threadiness; i++ {
-		go wait.Until(t.runWorker, time.Second, stopCh)
+		go wait.Until(pc.runWorker, time.Second, stopCh)
 	}
 	klog.Info("Started workers")
 	<-stopCh
@@ -74,31 +74,31 @@ func (t *PyTorchExtensionController) Run(threadiness int, stopCh <-chan struct{}
 	return nil
 }
 
-func (t *PyTorchExtensionController) runWorker() {
-	for t.processNextWorkItem() {
+func (pc *PyTorchExtensionController) runWorker() {
+	for pc.processNextWorkItem() {
 	}
 }
 
-func (t *PyTorchExtensionController) processNextWorkItem() bool {
-	obj, shutdown := t.workqueue.Get()
+func (pc *PyTorchExtensionController) processNextWorkItem() bool {
+	obj, shutdown := pc.workqueue.Get()
 	if shutdown {
 		return false
 	}
 
-	// We wrap this block in a func so we can defer t.workqueue.Done.
+	// We wrap this block in a func so we can defer pc.workqueue.Done.
 	err := func(obj interface{}) error {
-		defer t.workqueue.Done(obj)
+		defer pc.workqueue.Done(obj)
 		var key string
 		var ok bool
 
 		if key, ok = obj.(string); !ok {
-			t.workqueue.Forget(obj)
+			pc.workqueue.Forget(obj)
 			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
 			return nil
 		}
 
-		err := t.syncHandler(key)
-		t.handleErr(err, key)
+		err := pc.syncHandler(key)
+		pc.handleErr(err, key)
 
 		return nil
 	}(obj)
@@ -110,7 +110,7 @@ func (t *PyTorchExtensionController) processNextWorkItem() bool {
 	return true
 }
 
-func (t *PyTorchExtensionController) syncHandler(key string) error {
+func (pc *PyTorchExtensionController) syncHandler(key string) error {
 	// Convert the namespace/name string into a distinct namespace and name
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
@@ -118,11 +118,11 @@ func (t *PyTorchExtensionController) syncHandler(key string) error {
 		return err
 	}
 	// Get queueunit from cache
-	queueUnit, err := t.queueInformer.Lister().QueueUnits(namespace).Get(name)
+	queueUnit, err := pc.queueInformer.Lister().QueueUnits(namespace).Get(name)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			klog.Errorf("QueueUnit %s has been deleted: %s ...", queueUnit.Name)
-			// If can't get queueunit, return nil, handleErr function will forget key from workqueue
+			// If can'pc get queueunit, return nil, handleErr function will forget key from workqueue
 			return nil
 		}
 		runtime.HandleError(fmt.Errorf("failed to get queueunit by: %s/%s", namespace, name))
@@ -133,7 +133,7 @@ func (t *PyTorchExtensionController) syncHandler(key string) error {
 
 	if queueUnit.Status.Phase == v1alpha1.Dequeued {
 		klog.Infof("QueueUnit %v/%v has dequeued", queueUnit.Namespace, queueUnit.Name)
-		err = t.DeleteQueueAnotationInPytorchJob(queueUnit)
+		err = pc.deleteQueueAnotationInPytorchJob(queueUnit)
 		if errors.IsNotFound(err) {
 			// If can't find pytorchjob for queueunit, return err, handleErr function will requeue key MaxRetries times
 			return err
@@ -143,14 +143,14 @@ func (t *PyTorchExtensionController) syncHandler(key string) error {
 	return nil
 }
 
-func (t *PyTorchExtensionController) deleteQueueUnitWhenJobNotFound(key string) {
+func (pc *PyTorchExtensionController) deleteQueueUnitWhenJobNotFound(key string) {
 	namespace, name, err := cache.SplitMetaNamespaceKey(key)
 	if err != nil {
 		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
 		return
 	}
 
-	err = t.deleteQueueUnitInstance(namespace, name)
+	err = pc.deleteQueueUnitInstance(namespace, name)
 	if err != nil {
 		klog.Errorf("Delete queueunit error: %v/%v %v", namespace, name, err.Error())
 		return
@@ -158,79 +158,79 @@ func (t *PyTorchExtensionController) deleteQueueUnitWhenJobNotFound(key string) 
 	klog.Warningf("Delete queueunit %v/%v because can't find related pytorchjob ", namespace, name)
 }
 
-func (t *PyTorchExtensionController) handleErr(err error, key string) {
+func (pc *PyTorchExtensionController) handleErr(err error, key string) {
 	if err == nil {
-		t.workqueue.Forget(key)
+		pc.workqueue.Forget(key)
 		return
 	}
 
-	if t.workqueue.NumRequeues(key) < MaxRetries {
-		t.workqueue.AddRateLimited(key)
-		klog.Infof("We will requeue %v %d times,because:%v, has retried %d times", key, MaxRetries, err, t.workqueue.NumRequeues(key)+1)
+	if pc.workqueue.NumRequeues(key) < MaxRetries {
+		pc.workqueue.AddRateLimited(key)
+		klog.Infof("We will requeue %v %d times,because:%v, has retried %d times", key, MaxRetries, err, pc.workqueue.NumRequeues(key)+1)
 		return
 	}
 
 	runtime.HandleError(err)
 	klog.Infof("Dropping queueunit %q out of the workqueue: %v", key, err)
-	t.workqueue.Forget(key)
+	pc.workqueue.Forget(key)
 	// If still can't find job after retry, delete queueunit
-	t.deleteQueueUnitWhenJobNotFound(key)
+	pc.deleteQueueUnitWhenJobNotFound(key)
 }
 
-func (t *PyTorchExtensionController) enqueueQueueUnit(obj interface{}) {
+func (pc *PyTorchExtensionController) enqueueQueueUnit(obj interface{}) {
 	var key string
 	var err error
 	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
 		runtime.HandleError(err)
 		return
 	}
-	t.workqueue.AddRateLimited(key)
+	pc.workqueue.AddRateLimited(key)
 }
 
-func (t *PyTorchExtensionController) AddQueueUnit(obj interface{}) {
+func (pc *PyTorchExtensionController) AddQueueUnit(obj interface{}) {
 	qu := obj.(*v1alpha1.QueueUnit)
 	klog.Infof("Add queueunit:%v/%v", qu.Namespace, qu.Name)
-	t.enqueueQueueUnit(qu)
+	pc.enqueueQueueUnit(qu)
 }
 
-func (t *PyTorchExtensionController) UpdateQueueUnit(oldObj, newObj interface{}) {
+func (pc *PyTorchExtensionController) UpdateQueueUnit(oldObj, newObj interface{}) {
 	oldQu := oldObj.(*v1alpha1.QueueUnit)
 	newQu := newObj.(*v1alpha1.QueueUnit)
 	if oldQu.ResourceVersion == newQu.ResourceVersion {
 		return
 	}
-	t.enqueueQueueUnit(newQu)
+	pc.enqueueQueueUnit(newQu)
 }
 
-func (t *PyTorchExtensionController) DeleteQueueUnit(obj interface{}) {
+func (pc *PyTorchExtensionController) DeleteQueueUnit(obj interface{}) {
 	qu := obj.(*v1alpha1.QueueUnit)
 	klog.Infof("QueueUnit deleted:%v/%v", qu.Namespace, qu.Name)
 }
 
-func (t *PyTorchExtensionController) AddPytorchJob(obj interface{}) {
+func (pc *PyTorchExtensionController) AddPytorchJob(obj interface{}) {
 	pytorchJob := obj.(*pytorchjobv1.PyTorchJob)
 	klog.Infof("Add pytorchjob:%v/%v", pytorchJob.Namespace, pytorchJob.Name)
 }
 
-func (t *PyTorchExtensionController) UpdatePytorchJob(_, newObj interface{}) {
+func (pc *PyTorchExtensionController) UpdatePytorchJob(_, newObj interface{}) {
 	newJob := newObj.(*pytorchjobv1.PyTorchJob)
 	conditionsLen := len(newJob.Status.Conditions)
 	if conditionsLen > 0 {
 		lastCondition := newJob.Status.Conditions[conditionsLen-1]
 		if lastCondition.Type == commonv1.JobFailed || lastCondition.Type == commonv1.JobSucceeded {
 			klog.Infof("job %v/%v finished, current lastCondition.Type: [%v]", newJob.Namespace, newJob.Name, lastCondition.Type)
-			t.deleteQueueUnitAfterJobTerminated(newJob)
+			pc.deleteQueueUnitAfterJobTerminated(newJob)
 		}
 	}
 }
 
-func (t *PyTorchExtensionController) DeletePytorchJob(obj interface{}) {
+func (pc *PyTorchExtensionController) DeletePytorchJob(obj interface{}) {
 	job := obj.(*pytorchjobv1.PyTorchJob)
-	t.deleteQueueUnitAfterJobTerminated(job)
+	pc.deleteQueueUnitAfterJobTerminated(job)
 }
 
-func (t *PyTorchExtensionController) deleteQueueUnitAfterJobTerminated(job *pytorchjobv1.PyTorchJob) {
-	qulist, err := t.queueClient.SchedulingV1alpha1().QueueUnits(job.Namespace).List(context.TODO(), metav1.ListOptions{})
+func (pc *PyTorchExtensionController) deleteQueueUnitAfterJobTerminated(job *pytorchjobv1.PyTorchJob) {
+	qulist, err := pc.queueClient.SchedulingV1alpha1().QueueUnits(job.Namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		klog.Errorf("DeletePytorchJob error: get qulist failed %v/%v %v", job.Namespace, job.Name, err.Error())
 		return
@@ -238,7 +238,7 @@ func (t *PyTorchExtensionController) deleteQueueUnitAfterJobTerminated(job *pyto
 
 	for _, qu := range qulist.Items {
 		if qu.Spec.ConsumerRef.Name == job.Name {
-			err = t.deleteQueueUnitInstance(job.Namespace, qu.Name)
+			err = pc.deleteQueueUnitInstance(job.Namespace, qu.Name)
 			if err != nil {
 				klog.Errorf("Delete queueunit error: delete qu failed %v/%v %v", qu.Namespace, qu.Name, err)
 			}
@@ -247,8 +247,8 @@ func (t *PyTorchExtensionController) deleteQueueUnitAfterJobTerminated(job *pyto
 	}
 }
 
-func (t *PyTorchExtensionController) deleteQueueUnitInstance(namespace, name string) error {
-	err := t.queueClient.SchedulingV1alpha1().QueueUnits(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
+func (pc *PyTorchExtensionController) deleteQueueUnitInstance(namespace, name string) error {
+	err := pc.queueClient.SchedulingV1alpha1().QueueUnits(namespace).Delete(context.TODO(), name, metav1.DeleteOptions{})
 	if err != nil {
 		return err
 	}
@@ -256,16 +256,16 @@ func (t *PyTorchExtensionController) deleteQueueUnitInstance(namespace, name str
 	return nil
 }
 
-func (t *PyTorchExtensionController) DeleteQueueAnotationInPytorchJob(qu *v1alpha1.QueueUnit) error {
+func (pc *PyTorchExtensionController) deleteQueueAnotationInPytorchJob(qu *v1alpha1.QueueUnit) error {
 	namespace := qu.Spec.ConsumerRef.Namespace
 	pytorchJobName := qu.Spec.ConsumerRef.Name
-	pytorchJob, err := t.pytorchJobClient.KubeflowV1().PyTorchJobs(qu.Spec.ConsumerRef.Namespace).Get(context.TODO(), pytorchJobName, metav1.GetOptions{})
+	pytorchJob, err := pc.pytorchJobClient.KubeflowV1().PyTorchJobs(qu.Spec.ConsumerRef.Namespace).Get(context.TODO(), pytorchJobName, metav1.GetOptions{})
 	if err != nil {
 		if errors.IsNotFound(err) {
 			klog.Warningf("Can not find related pytorchjob:%v for queueunit:%v in namespace:%v", pytorchJobName, qu.Name, namespace)
 			return err
 		}
-		klog.Errorf("Get PytorchJob failed %v/%v %v", namespace, pytorchJobName, err.Error())
+		klog.Errorf("Get pytorchjob failed %v/%v %v", namespace, pytorchJobName, err.Error())
 		return err
 	}
 
@@ -278,9 +278,9 @@ func (t *PyTorchExtensionController) DeleteQueueAnotationInPytorchJob(qu *v1alph
 	pytorchJob.SetAnnotations(annotation)
 
 	// TODO change to patch
-	_, err = t.pytorchJobClient.KubeflowV1().PyTorchJobs(namespace).Update(context.TODO(), pytorchJob, metav1.UpdateOptions{})
+	_, err = pc.pytorchJobClient.KubeflowV1().PyTorchJobs(namespace).Update(context.TODO(), pytorchJob, metav1.UpdateOptions{})
 	if err != nil {
-		klog.Errorf("UpdateQueueUnit error: update PytorchJob failed %v/%v %v", namespace, pytorchJobName, err.Error())
+		klog.Errorf("UpdateQueueUnit error: update pytorchjob failed %v/%v %v", namespace, pytorchJobName, err.Error())
 		return err
 	}
 	klog.Infof("Update annotations for pytorchjob %v/%v", pytorchJob.Namespace, pytorchJob.Name)
